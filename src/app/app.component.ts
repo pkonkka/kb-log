84d97aa9aaccc82f7518fe1702e64bb3d2699d21
Petri Konkka <petrikonkka@gmail.com>
2017-03-13 15:55:37 +0200

    basic menu added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { MenuController, NavController, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { WorkoutsPage } from '../pages/workouts/workouts';
+import { ExercisesPage } from '../pages/exercises/exercises';
+import { CategoriesPage } from '../pages/categories/categories';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = WorkoutsPage;
+  workoutsPage = WorkoutsPage;
+  exercisesPage = ExercisesPage;
+  categoriesPage = CategoriesPage;
 
-  constructor(platform: Platform) {
+  @ViewChild('nav') nav: NavController;
+
+  // ---------------------------------------------------------------------------------------------
+  constructor(platform: Platform, private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +27,12 @@ export class MyApp {
       Splashscreen.hide();
     });
   }
+
+  // ---------------------------------------------------------------------------------------------
+  onLoad(page: any) {
+    console.log('page: ', page);
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { WorkoutsPage } from '../pages/workouts/workouts';
+import { ExercisesPage } from '../pages/exercises/exercises';
+import { CategoriesPage } from '../pages/categories/categories';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    WorkoutsPage,
+    ExercisesPage,
+    CategoriesPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +21,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    WorkoutsPage,
+    ExercisesPage,
+    CategoriesPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
