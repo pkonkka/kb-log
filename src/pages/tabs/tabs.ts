a7709d38ba1413c5492c6d99fad11a25dd794e61
Petri Konkka <petrikonkka@gmail.com>
2017-03-13 19:20:32 +0200

    most of the menu items moved to tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 
+import { HomePage } from '../home/home';
+import { ExercisesPage } from '../exercises/exercises';
+import { WorkoutsPage } from '../workouts/workouts';
 import { StatsPage } from '../stats/stats';
-import { SettingsPage } from '../settings/settings';
 
 
 @Component({
     selector: 'page-tabs',
     template: `
         <ion-tabs>
-            <ion-tab [root]="statsPage" tabTitle="Stats" tabIcon="star"></ion-tab>
-            <ion-tab [root]="settingsPage" tabTitle="Settings" tabIcon="book"></ion-tab>
+            <ion-tab [root]="homePage" tabIcon="home"></ion-tab>                    
+            <ion-tab [root]="workoutsPage" tabTitle="Workouts" tabIcon="list"></ion-tab>        
+            <ion-tab [root]="exercisesPage" tabTitle="Exercises" tabIcon="body"></ion-tab>                    
+            <ion-tab [root]="statsPage" tabTitle="Stats" tabIcon="stats"></ion-tab>
         </ion-tabs>
     `
 })
 export class TabsPage {
+    homePage = HomePage;
+    workoutsPage = WorkoutsPage;
+    exercisesPage = ExercisesPage;
     statsPage = StatsPage;
-    settingsPage = SettingsPage;
+
 }
