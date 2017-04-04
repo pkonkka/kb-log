f5fdde70af0e63b298ee286efd31ad530f7bcf76
Petri Konkka <petrikonkka@gmail.com>
2017-04-04 15:00:45 +0300

    Static file path changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static('www'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
